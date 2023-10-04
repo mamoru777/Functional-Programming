46beb99b929e9b9c74c0c3d7a1ce424e12ab1acf
Mamoru777 <senkinsaha@mail.ru>
2023-10-04 22:45:49 +0400

    Добавьте файлы проекта.

---
+++ Lab1/Program.fs
@@ -0,0 +1,11 @@
+﻿let isNotMultipleOf3AndMultipleOf5 (n: int) =
+    n % 3 <> 0 && n % 5 = 0
+
+let numbers = seq { 1..100 }
+
+let result =
+    numbers
+    |> Seq.filter isNotMultipleOf3AndMultipleOf5
+    |> Seq.sum
+
+printfn "Сумма чисел, некратных 3 и кратных 5, от 1 до 100: %d" result
