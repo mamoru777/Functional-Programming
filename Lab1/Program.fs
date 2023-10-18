e077c5dc4a91818351c89cae59398125578ad114
Mamoru777 <senkinsaha@mail.ru>
2023-10-18 15:43:13 +0400

    Lab2

--- Lab1/Program.fs
+++ Lab1/Program.fs
@@ -1,11 +1,30 @@
-﻿let isNotMultipleOf3AndMultipleOf5 (n: int) =
-    n % 3 <> 0 && n % 5 = 0
+﻿open System.IO
+open System.Text.RegularExpressions
 
-let numbers = seq { 1..100 }
+// Функция для чтения текста из файла
+let readTextFromFile (filePath: string) =
+    use reader = File.OpenText(filePath)
+    reader.ReadToEnd()
 
-let result =
-    numbers
-    |> Seq.filter isNotMultipleOf3AndMultipleOf5
-    |> Seq.sum
+// Фопция для определения самого часто встречающегося слова в тексте
+let findMostCommonWord (text: string) =
+    let wordPattern = @"\b\w+\b" // Регулярное выражение для слов
+    let matches = Regex.Matches(text, wordPattern)
 
-printfn "Сумма чисел, некратных 3 и кратных 5, от 1 до 100: %d" result
+    let wordCounts =
+        matches
+        |> Seq.cast<Match>
+        |> Seq.map (fun m -> m.Value)
+        |> Seq.groupBy id
+        |> Seq.map (fun (word, group) -> (word, Seq.length group))
+        |> Seq.sortByDescending (fun (_, count) -> count)
+    
+    let mostCommonWord, mostCommonCount = Seq.head wordCounts
+    
+    mostCommonWord, mostCommonCount
+
+let textFileName = "text_file.txt"
+let text = readTextFromFile textFileName
+let mostCommonWord, mostCommonCount = findMostCommonWord text
+
+printfn "Самое часто встречающееся слово: %s (встречается %d раз)" mostCommonWord mostCommonCount
