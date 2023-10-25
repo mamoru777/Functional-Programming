d91ed961192669898bfd2dae2e992eead2abd4cc
Mamoru666 <senkinsaha@mail.ru>
2023-10-25 18:52:05 +0400

    Lab4

--- Lab1/Program.fs
+++ Lab1/Program.fs
@@ -1,28 +1,34 @@
-﻿// Интерфейс для сотрудников деканата
-type IDekanatEmployee =
+﻿type IDekanatEmployee =
     abstract member GetName : unit -> string
     abstract member GetPosition : unit -> string
 
-// Базовый класс для сотрудников деканата
 type DekanatEmployee(name: string, position: string) =
     interface IDekanatEmployee with
         member this.GetName () = name
         member this.GetPosition () = position
 
-// Класс для студентов
 type Student(name: string, major: string) =
     inherit DekanatEmployee(name, "Студент")
     member this.GetMajor () = major
-    //member this.GetName () = name
 
-// Класс для преподавателей
+    interface IDekanatEmployee with
+        member this.GetName () = name
+        member this.GetPosition () = "Студент"
+
 type Teacher(name: string, department: string) =
     inherit DekanatEmployee(name, "Преподаватель")
     member this.GetDepartment () = department
+
+    interface IDekanatEmployee with
+        member this.GetName () = name
+        member this.GetPosition () = "Преподаватель"
 
 // Пример использования
 let student = new Student("Иван", "Математика")
 let teacher = new Teacher("Анна", "Физика")
 
-printfn "Студент: %s, Отделение: %s" (student.GetName ()) (student.GetMajor ())
-printfn "Преподаватель: %s, Отделение: %s" (teacher.GetName ()) (teacher.GetDepartment ())
+let printEmployeeInfo (employee: IDekanatEmployee) =
+    printfn "Сотрудник: %s, Должность: %s" (employee.GetName ()) (employee.GetPosition ())
+
+printEmployeeInfo student
+printEmployeeInfo teacher
